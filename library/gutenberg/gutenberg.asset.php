92c52539cb4d26b7fd534afc90e2d0d07c15ca44
gtgtgl <ykr20141101@outlook.jp>
2024-10-28 15:57:03 +0900

    gutenbergブロックエディタの挙動改善

--- library/gutenberg/gutenberg.asset.php
+++ library/gutenberg/gutenberg.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'ec30f5ebf2931f76b645');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '7cee51e719452b23f657');
